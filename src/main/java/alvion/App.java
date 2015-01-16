d895c1d2d57a86523f206e2a263b6ea7a340bf1d
dzhdan <dzhdan@alvioneurope.ru>
2015-01-16 10:35:15 +0300

    dzhdan: translit application

--- src/main/java/alvion/App.java
+++ src/main/java/alvion/App.java
@@ -1,14 +1,15 @@
 package alvion;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello Alvion!" );
+    public static void main( String[] args ) throws IOException {
+        Translit translit = new Translit();
+        translit.run();
     }
 }
