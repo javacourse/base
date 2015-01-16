fff19a3d2ab3c36d14b320e61f13848ca04dfe07
ragham <ramose@mail.ru>
2015-01-16 13:36:13 +0400

    First Commit

--- src/main/java/alvion/App.java
+++ src/main/java/alvion/App.java
@@ -1,14 +1,22 @@
 package alvion;
 
 /**
- * Hello world!
- *
+ * Created by egetman on 1/15/2015.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello Alvion!" );
+import java.util.Scanner;
+
+public  class  App{
+
+    public  static void  main(String[] args)  {
+        Scanner  input=  new  Scanner(System.in);
+
+        //RusToEng rte =  new RusToEng();
+
+        do  {
+            System.out.println ("\n  Enter  Rus phrase :  "  );
+            String phrase =  input.nextLine();//Read Line
+            String engPhrase =  RusToEng.rusToEng (phrase) ;//Use method RusToEng class rusToEng
+            System.out.println("  You phrase in Eng : \n" + engPhrase);// output result
+        }  while  (true);
     }
 }
