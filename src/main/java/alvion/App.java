b37f0db95eb7ab8457ecdbf236bd78e0af7dfd04
mhorok <Mykyta.Horokhovtsev@medhost.com>
2015-01-16 11:22:50 +0300

    transliterate task done

--- src/main/java/alvion/App.java
+++ src/main/java/alvion/App.java
@@ -1,14 +1,17 @@
 package alvion;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello Alvion!" );
+    public static void main( String[] args ) throws IOException {
+        Transliterator transliterator  = new Transliterator();
+
+        transliterator.readLine();
+        transliterator.writeLine();
     }
 }
