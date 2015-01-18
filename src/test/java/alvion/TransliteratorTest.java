e4e6dd6f0d984957b2886d6128f86d12ca564ea0
Max Bagrov <wisdom683@rambler.ru>
2015-01-18 18:41:59 +0300

    Added first unit test for class Transliterator, fixed .gitignore file

---
+++ src/test/java/alvion/TransliteratorTest.java
@@ -0,0 +1,34 @@
+package alvion;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransliteratorTest {
+
+    private Transliterator transliterator;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("setUp");
+        transliterator = new Transliterator();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("tearDown");
+    }
+
+    @Test
+    public void testTransliterateString() throws Exception {
+        System.out.println("testTransliterateString started...");
+        String messageToTransliterate = "Щука. КАРАСЬ. ЛоСоСЬ. Whale. $#&%.";
+        String correctMessage = "Shhuka. KARAS. LoSoS. Whale. $#&%.";
+
+        String transliteratedMessage = transliterator.transliterateString(messageToTransliterate);
+
+        assertEquals(transliteratedMessage, correctMessage);
+    }
+}
