ec983e29932b25e31daa28809addfb652de451f3
ragham <ramose@mail.ru>
2015-01-21 23:05:01 +0300

    Added comments

--- src/main/java/alvion/App.java
+++ src/main/java/alvion/App.java
@@ -11,9 +11,9 @@ public  class  App{
     public  static void  main(String[] args)  {
         Scanner  input=  new  Scanner(System.in);
         ArrayList<String> tree= new ArrayList();
-        SearchString searchString = new SearchString();
-        RusToEng rusToEng = new RusToEng();
-        //System.out.println("searchString: "+ searchString.SearchString("aaaaaaaaaaaaa","aaa"));
+        SearchString searchString = new SearchString();//
+        RusToEng rusToEng = new RusToEng();//Class transliteration
+
         tree.add("Appendable");
         tree.add("ArithmetricException");
         tree.add("ArrayndexOfBoundsException");
@@ -22,35 +22,20 @@ public  class  App{
         tree.add("AutoCloseable");
         tree.add("Boolean");
 
-        /*int totalElem = tree.size();
-        for (int i=0; i<totalElem; i++) {
-            System.out.println("Array element["+i+"]: " + tree.get(i));// output result
-
-        }*/
-
-
-
-
         do  {
             System.out.println ("\n  Enter phrase for search:  "  );
-            String phrase =  RusToEng.rusToEng(input.nextLine());//Read Line
-            //String phrase =  input.nextLine();//Read Line
-
+            String phrase =  RusToEng.rusToEng(input.nextLine());//Read Line and transliterate
+            //if line isEmpty Exit
             if (!phrase.equals("")) {
                 int totalElem = tree.size();
 
-                //tree.get(1).contains(phrase);
-                    for (int i=0; i<totalElem; i++) {
-                        if (true == searchString.SearchString(tree.get(i),phrase)
-                        //tree.get(i).toLowerCase().contains(phrase.toLowerCase())
-                        ) {
+                for (int i=0; i<totalElem; i++) {
+                        if (true == searchString.SearchString(tree.get(i),phrase)) {
                             System.out.println("  Array element[" + i + "] contains " + tree.get(i));// output result
                         }else{
                             System.out.println("  Array element[" + i + "] doesn't contain: " + tree.get(i));
                         }
                     }
-
-
                 //String engPhrase = RusToEng.rusToEng(phrase);//Use method RusToEng class rusToEng
                 //System.out.println("  You phrase in Eng : \n" + engPhrase);// output result
             }else       {
--- src/main/java/alvion/SearchString.java
+++ src/main/java/alvion/SearchString.java
@@ -12,22 +12,18 @@ public class SearchString {
        char[] lineitem = item.toCharArray();
        boolean flag;
 
-
+        //if first symbol is UpperCase we will find by UpperCase Symbols
         if(Character.isUpperCase(linetext[0])){
             StringBuilder builder = new StringBuilder();
-            String line; // = new char[10];
-            String str;
-
+            String line; //Additional variable
+            //select only UpperCase Symbols
             for(int i = 0; i < lineitem.length; i++) {
-
                 if (Character.isUpperCase(lineitem[i])) {
                     builder.append(lineitem[i]);
-
                 }
             }
-
             line = builder.toString();
-            flag = line.toUpperCase().contains(text.toUpperCase());
+            flag = line.contains(text);
             //System.out.print(" line = "+line.toString()+" text= "+text +" Flag= "+flag);
         }else{
             flag = item.toLowerCase().contains(text.toLowerCase());
