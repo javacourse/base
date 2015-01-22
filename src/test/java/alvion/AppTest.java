58c9eb3d8034ac0e5c415d4f708f24e8e5c97326
axelsvjax <axelsvjax@gmail.com>
2015-01-22 14:39:53 +0300

    Added files test.

--- src/test/java/alvion/AppTest.java
+++ src/test/java/alvion/AppTest.java
@@ -4,6 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
@@ -24,5 +29,19 @@ public class AppTest {
         final String message = app.helloWorld();
         System.out.println("Hello world!");
         assertEquals("Hello world!", message);
+    }
+
+    @Test
+    public void testPaths() throws Exception {
+        Path path = Paths.get("D:/test/testfile.txt");
+        Files.createFile(path);
+        assertTrue(Files.exists(path));
+    }
+
+    @Test
+    public void testFiles() throws Exception {
+        File file = new File("D:\\test\\testfile2.txt");
+        file.createNewFile();
+        assertTrue(file.exists());
     }
 }
